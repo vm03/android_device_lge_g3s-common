a6c7399d91dac86761f11981710fd54568f50c61
vm03 <vm03@yandex.ru>
2015-11-12 10:46:18 +0300

    add keystore
    Change-Id: I97c541e62813386b99a36a3f123f2bbacf7f5c57

--- device.mk
+++ device.mk
@@ -82,6 +82,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.lcd_density=320
 
+# Keystore
+PRODUCT_PACKAGES += \
+    keystore.msm8226
+
 # Keyhandler
 PRODUCT_PACKAGES += \
     com.cyanogenmod.keyhandler
